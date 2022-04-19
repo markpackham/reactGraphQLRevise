cf4dcaf770aa351ca19f9dd41381edc9bc588e3f
mark <markpackham1@gmail.com>
2022-04-19 21:03:32 +0100

    create UserType
    https://youtu.be/Dr2dDWzThK8?t=960

--- server/index.js
+++ server/index.js
@@ -5,8 +5,23 @@ const cors = require("cors");
 const userData = require("./MOCK_DATA.json");
 const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString} = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLInt},
+    firstName: { type: GraphQLString},
+    lastName: { type: GraphQLString},
+    email: { type: GraphQLString},
+    password: { type: GraphQLString},
+  })
+})
 
-const RootQuery = "query";
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    
+  }
+});
 const Mutation = "mutation";
 
 const schama = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
