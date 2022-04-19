61a31d0c75741d04d664a4b306115fd773543aac
mark <markpackham1@gmail.com>
2022-04-19 17:17:07 +0100

    add placeholders for schema
    https://youtu.be/Dr2dDWzThK8?t=617

--- server/index.js
+++ server/index.js
@@ -4,8 +4,22 @@ const PORT = 6969;
 const cors = require("cors");
 const userData = require("./MOCK_DATA.json");
 const graphql = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const RootQuery = "query";
+const Mutation = "mutation";
+
+const schama = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
 app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log("Server running");
