ee3c6481df7023fd527db0b6f976cdf95fd35360
mark <markpackham1@gmail.com>
2022-04-20 14:34:09 +0100

    get users

--- client/src/components/GetUsers.js
+++ client/src/components/GetUsers.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+import { LOAD_USERS } from "../GraphQL/Queries";
 
 function GetUsers() {
+  const { error, loading, data } = useQuery(LOAD_USERS);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return <div>GetUsers</div>;
 }
 
