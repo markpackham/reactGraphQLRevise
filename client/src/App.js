349cd6ac45f8f40d62ffef66d9bb52c51dd1a1a0
mark <markpackham1@gmail.com>
2022-04-20 14:37:51 +0100

    output data in console
    https://youtu.be/YyUWW04HwKY?t=1025

--- client/src/App.js
+++ client/src/App.js
@@ -7,29 +7,32 @@ import {
   from,
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
+import GetUsers from "./components/GetUsers";
 
 const link = from([
   errorLink,
-  new HttpLink({uri: "http://localhost:6969/graphql"})
-])
+  new HttpLink({ uri: "http://localhost:6969/graphql" }),
+]);
 
-const errorLink = onError(({graphqlErrors, networkError})=>{
-  if(graphqlErrors){
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
     graphqlErrors.map(({ message, location, path }) => {
       alert(`Graphql error ${message}`);
     });
   }
-})
+});
 
 const client = new ApolloClient({
-  cache: new InMemoryCache,
-  link: 
+  cache: new InMemoryCache(),
+  link: link,
 });
 
 function App() {
-  return <ApolloProvider client={client}>
-
-  </ApolloProvider>;
+  return (
+    <ApolloProvider client={client}>
+      <GetUsers />
+    </ApolloProvider>
+  );
 }
 
 export default App;
