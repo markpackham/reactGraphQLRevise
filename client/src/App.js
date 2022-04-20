fc2994e359e33ed1298707d9b98ab6f0d345dfdd
mark <markpackham1@gmail.com>
2022-04-20 13:22:24 +0100

    add link and errorLink for client end
    https://youtu.be/YyUWW04HwKY?t=520

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,18 @@ import {
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 
+const link = from([
+  errorLink,
+  new HttpLink({uri: "http://localhost:6969/graphql"})
+])
+
+const errorLink = onError(({graphqlErrors, networkError}))
+
+const client = new ApolloClient({
+  cache: new InMemoryCache,
+  link: 
+});
+
 function App() {
   return <div>This works</div>;
 }
