e286858c5fdc20df6a3319539802828bc8aeae9d
mark <markpackham1@gmail.com>
2022-04-20 14:49:26 +0100

    output first user firstname
    https://youtu.be/YyUWW04HwKY?t=1167

--- client/src/components/GetUsers.js
+++ client/src/components/GetUsers.js
@@ -1,15 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { LOAD_USERS } from "../GraphQL/Queries";
 
 function GetUsers() {
   const { error, loading, data } = useQuery(LOAD_USERS);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    console.log(data);
+    if (data) {
+      setUsers(data.getAllUsers[0]);
+    }
   }, [data]);
 
-  return <div>GetUsers</div>;
+  return (
+    <div>
+      {users.map((val) => {
+        return <h1>{val.firstName}</h1>;
+      })}
+    </div>
+  );
 }
 
 export default GetUsers;
